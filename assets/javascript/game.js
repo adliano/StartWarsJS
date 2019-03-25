37294e53619f60996b7812fe68056ce6dfd77736
Adliano <adliano@me.com>
2019-03-24 22:22:14 -0700

    added mkvisible and mkinvisible functions

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -13,6 +13,26 @@ window.onload = function () {
     // Get total numbefr of images
     let totalCharacters = characters.length;
 
+    /* ***************************************************** */
+    /* * * * * * * * * * * mkInvisible() * * * * * * * * * * */
+    /* ***************************************************** */
+    // Method to make element invisible using element ID(s)
+    let mkInvisible = function(...elementIDs){
+        for(let element of elementIDs ){
+            //document.getElementById(element).classList.add("invisible");
+            document.querySelector(element).classList.add("invisible")
+        }
+    }
+    /* ***************************************************** */
+    /* * * * * * * * * * * * mkVisible() * * * * * * * * * * */
+    /* ***************************************************** */
+    // Method to make element visible using element ID(s)
+    let mkVisible = function(...elementIDs){
+        for(let element of elementIDs ){
+            // document.getElementById(element).classList.remove("invisible");
+            document.querySelector(element).classList.remove("invisible")
+        }
+    }
     // Add onclick listener to attackes
     for (let attacker of characters) {
         attacker.addEventListener("click", function (event) {
@@ -22,10 +42,14 @@ window.onload = function () {
                 attackerName = event.target.getAttribute("alt");
                 // Remove Selected Attacker from Array (splice returns HTML collection)
                 currentAttacker = characters.splice(characters.indexOf(event.target), 1);
-                // Move the enemyCollumn other characters to 
+                // Move the enemiesCollumn other characters to 
                 for (let _enemy of characters) {
-                    document.querySelector("#enemyCollumn").append(_enemy);
+                    // TODO: change background color to yellow
+                    // ................
+                    document.querySelector("#enemiesCollumn").append(_enemy);
                 }
+                // Display availableEnemiesHeader 
+                mkVisible("#availableEnemiesHeader");
                 // change attackerNotSelected flag
                 attackerNotSelected = !attackerNotSelected;
             }
@@ -35,8 +59,21 @@ window.onload = function () {
                 enemyName = event.target.getAttribute("alt");
                 // Remove Selected Enemy From Array (splice returns HTML collection)
                 currentEnemy = characters.splice(characters.indexOf(event.target), 1);
-                // Move Selected Enemy to ....
-                document.querySelector("#test").append(currentEnemy[0]);
+                // TODO: Change bg color to red
+                // ...............
+                // Move Selected Enemy to enemyColumn
+                document.querySelector("#enemyColumn").append(currentEnemy[0]);
+                // remove available enemies durring fight section
+                mkInvisible("#availableEnemiesHeader","#enemiesCollumn");
+                // display fightSectionHeader, btnAttack and enemyHeader
+                mkVisible("#fightSectionHeader","#btnAttack","#enemyHeader");
+
+
+
+
+
+
+
                 // Change enemyNotSelected flag
                 enemyNotSelected = !enemyNotSelected;
             }
